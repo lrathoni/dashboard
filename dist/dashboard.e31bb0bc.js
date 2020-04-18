16ccf2240358a03b5c76ca0dc6096b3cda3ed6ae
lrathoni <line.jpn25@gmail.com>
2020-04-18 17:39:50 +0200

    correction folder

--- dist/dashboard.e31bb0bc.js
+++ dist/dashboard.e31bb0bc.js
@@ -147,7 +147,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "37667" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "43473" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
