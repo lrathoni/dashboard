38f2b8a37d98e6f1ecf14be40db6296448bedf53
victorine <victorine.maurel@hotmail.fr>
2020-04-20 15:05:14 +0200

    state : ajout object director et film

--- src/state/index.js
+++ src/state/index.js
@@ -1,35 +1,16 @@
 export default {
-    // Example
-    // https://api.themoviedb.org/3/movie/550?api_key=f88f1caf38ec9a3acf3d6c51b4bf9820
-    movieItems: [
-        {
-            title: 'Fight Club', // <=>  title or original_title
-            genres: 'Drama', // <=> genres //Sort by categories
-            populatiry: 41.719, // <=> popularity or vote_count => Make top films/serie
-            countries: ['Germany', 'United States of America'], // <=> production_countries
-            id: '550', // <=> id
-            release_date: '1999-10-15' // <=> released_date
-        },
-        {
-            title: 'The Poseidon Adventure Collection',
-            genres: ['Action', 'Adventure', 'Drama'],
-            populatiry: 9.715,
-            countries: ['United States of America'],
-            id: '551',
-            release_date: '1972-12-13'
-        }
-    ],
-    addMovieItemInput: '',
+  Director: {
+    id:'';
+    name: '';
+    films: [];
+    bio:'';
+  }
 
-    // Example
-    // https://api.themoviedb.org/3/tv/550?api_key=f88f1caf38ec9a3acf3d6c51b4bf9820
-    tvItems: {
-        title: 'Top Gear', // <=>  title or original_title
-        genres: 'Documentary', // <=> genres //Sort by categories
-        populatiry: 29.6, // <=> popularity or vote_count => Make top films/serie
-        countries: 'GB', // <=> origin_country WARNING just initial (England = 'en')
-        id: '45', // <=> Not id
-        release_date: '2002-10-20' // <=> first_air_date
-    },
-    addTvItems: ''
+  Film: {
+    id:'';
+    title:'';
+    genre:'';
+    year:'';
+    popularity:'';
+  }
 }
