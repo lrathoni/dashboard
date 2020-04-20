6f49d87aa732187c0e4697548bcdc5507f522126
victorine <victorine.maurel@hotmail.fr>
2020-04-20 15:27:02 +0200

    ajout liste director dans le state

--- src/state/index.js
+++ src/state/index.js
@@ -5,12 +5,25 @@ export default {
     films: [],
     bio:''
   },
-
   Film:  {
     id:'',
     title:'',
     genre:'',
     year:'',
     popularity:''
+  },
+  directorslist: {
+    'James Cameron': 2710,
+    'Wes Anderson': 5655,
+    'Michael Bay': 865,
+    'Luc Besson': 59,
+    'Danny Boyle': 2034,
+    'Christopher Nolan': 525,
+    'Stanley Kubrick': 240,
+    'David Lynch': 5602,
+    'David Fincher': 7467,
+    'Jean-Luc Godard': 3776,
+    'Peter Jackson': 108,
+    'Rocco Siffredi': 7694
   }
 }
