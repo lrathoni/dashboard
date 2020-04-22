be51985d3a61af7893f5a5d07b43a9af91fc8f08
victorine <victorine.maurel@hotmail.fr>
2020-04-22 14:11:54 +0200

    en sélectionnant réal champs name et id Director se remplissent

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,10 @@
 export default{
-    display : () => {
-        console.log('change')
-    }
+    onInputChange: (event) => state => {
+        state.Director.name = event.target.value
+        state.directorsList.map(item =>{
+        	if(item.name === state.Director.name){
+        		state.Director.id = item.id
+        	}
+        })
+      },
 }
--- src/components/view/view.js
+++ src/components/view/view.js
@@ -1,6 +1,6 @@
 import { h } from 'hyperapp'
 
-export default (state, actions) => h('select', {onchange: actions.display}, [
+export default (state, actions) => h('select', {onchange :(e)=>actions.onInputChange(e)}, [
     state.directorsList
         .map(item => h('option', {}, item.name))
 ])
